9ed51fce7420cd6c90b690d617c3ef3f42f228e2
fatememajdi <cache --timeout=86400>
2020-12-19 12:53:05 -0800

    OS8

---
+++ OS8/01.sh
@@ -0,0 +1,6 @@
+git config --global user.email
+git config --global user.email "fatemehmajdiii@gmail.com"
+git config --global user.name "fatememajdi"
+git config --global credential.helper cache
+git config --global user.email 'cache --timeout=86400'
+
