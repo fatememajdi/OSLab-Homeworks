270a38e37e3a6e9347b144866abdb499f7a50159
Fateme <76264392+fatememajdi@users.noreply.github.com>
2021-01-06 21:08:06 +0330

    Update 03.sh

--- OS10/03.sh
+++ OS10/03.sh
@@ -2,8 +2,8 @@
 
 echo direction:
 read D
-count=1
+cnt=1
 for file in $(find $D -type f -name "*.jpg" -or -type f -name "*.png");
 do
- mv $file $D/img$((count++))
+ mv $file $D/img$((cnt++))
 done 
